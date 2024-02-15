1957219c19b887519e939ed04d598d8db427cd69
amittiwari05 <160128473+amittiwari05@users.noreply.github.com>
2024-02-15 21:10:30 +0530

    Create swapfunction.py

---
+++ swapfunction.py
@@ -0,0 +1,17 @@
+x = 3
+y = 5
+
+print("BEFORE SWAP, value of x is:" ,x)
+print("BEFORE SWAP, value of y is:" ,y)
+temp = x
+print("value of temp is :" ,temp)
+x = y
+print("value of x is :" ,x)
+y = temp
+
+print("value of y is:" ,y)
+
+print("AFTER SWAP, value of x is",x)
+print("AFTER SWAP, value of y is",y)
+#make a program to find square root of a number
+#find the area of a triangle
